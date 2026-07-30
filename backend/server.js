e0a924d1388c73c12c31e2fd2ed3840564e07738
Sanchay-Kumar-Singh <sanchaysingh62425@gmail.com>
2026-07-30 12:38:24 +0530

    merge remote changes

--- backend/server.js
+++ backend/server.js
@@ -6,23 +6,41 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-app.use("/",(req,res)=>{
-  res.end("server is live!")
-})
+
+
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://ai-blog-platform-using-mern-stack.vercel.app",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Server is live!");
+});
+
 
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/blogs", require("./routes/blogRoutes"));
 app.use("/api/ai", require("./routes/aiRoutes"));
 
+
 // MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error("MongoDB Error:", err));
+  .catch((err) => console.log("MongoDB Error:", err));
 
+
+// Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
